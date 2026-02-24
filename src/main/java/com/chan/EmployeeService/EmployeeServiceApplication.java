21286f482e0db0fc68424f2408ad40f6c65c8236
chaniji <chandramoulim1921@gmail.com>
2026-02-24 23:03:51 +0530

    Project Setup

---
+++ src/main/java/com/chan/EmployeeService/EmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.chan.EmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    SpringApplication.run(EmployeeServiceApplication.class, args);
+  }
+
+}
