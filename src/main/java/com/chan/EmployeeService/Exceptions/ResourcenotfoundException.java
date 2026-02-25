6626ff05eeefb5d7b3a1c2a77d599dc8db3fb0fd
chaniji <chandramoulim1921@gmail.com>
2026-02-25 19:45:39 +0530

    Exceptions Added

---
+++ src/main/java/com/chan/EmployeeService/Exceptions/ResourcenotfoundException.java
@@ -0,0 +1,9 @@
+package com.chan.EmployeeService.Exceptions;
+
+public class ResourcenotfoundException extends RuntimeException {
+   public ResourcenotfoundException (String message){
+      super(message)
+
+
+   }
+}
